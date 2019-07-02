9f4a924675cfc4b7fe9fccde4b6852d4ababd26e
YukakoNakayama <sweetsky2089@gmail.com>
2019-07-02 14:24:57 +0900

    slick課題完成

--- jquery/slick/common.js
+++ jquery/slick/common.js
@@ -1,9 +1,12 @@
+$(function(){
   $('#slider').slick({
    // ここにオプションを追記する
-    //autoplay: true,
-    //arrows: true,
-    //focusOnSelect: true,
+    autoplay: true,
+    arrows: true,
+    dots: true,
+    focusOnSelect: true,
     centerPadding: '30%',
     focusOnSelect: true,
     speed: 1000,
   });
+});
